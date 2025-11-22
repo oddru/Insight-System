41848c5e43a094a9cbc0f588db4a4c75c0d4cf62
unknown <valcranium@gmail.com>
2025-11-23 00:23:43 +0800

    before proteg database

--- insightproject/accounts/views.py
+++ insightproject/accounts/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login 
+from django.contrib.auth import login
+from django.contrib.auth import authenticate, logout
 
 def register(request):
     if request.method == 'POST':
@@ -16,6 +17,23 @@ def register(request):
         else:
             form = UserCreationForm()
         return render(request, 'users/register.html', {'form':form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home') #return to home if not
+    return render(request, 'user/login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('home') #return to home if log out
+
+def user_home(request):
+    return render(request, "user/home.html")
     
     
         
