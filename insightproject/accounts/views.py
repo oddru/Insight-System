969c66b273128ac43e1fa63cb61c6f209e0195f4
Gentsy13 <ajlomocso235@gmail.com>
2025-11-22 23:06:35 +0800

    20 mins real

--- insightproject/accounts/views.py
+++ insightproject/accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login 
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home') # Return to home page
+        else:
+            form = UserCreationForm()
+        return render(request, 'users/register.html', {'form':form})
+    
+    
+        
